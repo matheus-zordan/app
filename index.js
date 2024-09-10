ae1637092e568e18d558f63bc6437562ce377db5
Matheus de Assis Zordan <83818797+matheus-zordan@users.noreply.github.com>
2024-09-10 10:49:46 -0300

    praticando

---
+++ index.js
@@ -0,0 +1,11 @@
+let metas = ["matheus","oi"]
+console.log(metas[0])
+console.log(metas[1] + ", " + metas[0])
+
+let meta = {
+    nome: 'Matheus',
+    idade: 27,
+    profissao: 'programador',
+}
+
+console.log(meta)
